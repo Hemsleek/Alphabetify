5a86bc7010aee21e57b7a5cab94fc1ad5489e03d
hemsleek <asiyanbimubashir@gmail.com>
2020-08-28 21:35:18 +0100

    feat: triggers

--- alphabetify.js
+++ alphabetify.js
@@ -1,4 +1,14 @@
-window.addEventListener("keydown", e => {
-    console.log(e)
-        // console.log(document.querySelector("audio")[src = "sounds/letters_sounds.mp3"])
-});
+ /* selectors */
+ window.addEventListener("keydown", e => {
+     let key = e.key.toLowerCase();
+     const audioEl = document.querySelector(`audio[data-key='${key}']`)
+     console.log(document.querySelector(`audio[data-key='${key}']`))
+     if (!audioEl) return
+     audioEl.currentTime = 0;
+     audioEl.play();
+     //  console.log(audioEl)
+
+     // document.querySelector('audio[src = "sounds/letters_sounds.mp3"]').play();
+
+
+ });
