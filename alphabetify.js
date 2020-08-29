597663b19d1ab31b285ac4b98934893484789497
hemsleek <asiyanbimubashir@gmail.com>
2020-08-29 15:43:42 +0100

    fix: triggers update

--- alphabetify.js
+++ alphabetify.js
@@ -1,14 +1,30 @@
  /* selectors */
+
+ /* triggers and event listeners*/
+ const alphabetEl = document.querySelectorAll("span");
+ alphabetEl.forEach(alphabet => {
+     alphabet.addEventListener('click', e => {
+         let key = e.target.innerText.toLowerCase();
+         console.log(key);
+
+         const audioEl = document.querySelector(`audio[data-key='${key}']`)
+         if (!audioEl) return
+         audioEl.currentTime = 0;
+         audioEl.play();
+     })
+ })
+
+
  window.addEventListener("keydown", e => {
      let key = e.key.toLowerCase();
      const audioEl = document.querySelector(`audio[data-key='${key}']`)
-     console.log(document.querySelector(`audio[data-key='${key}']`))
+
      if (!audioEl) return
      audioEl.currentTime = 0;
      audioEl.play();
-     //  console.log(audioEl)
+     //  }
 
-     // document.querySelector('audio[src = "sounds/letters_sounds.mp3"]').play();
+
 
 
  });
